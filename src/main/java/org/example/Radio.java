0fe8408a405f7ee300284f0abf19659a184393c0
Elya <salmanovaeln@yandex.ru>
2024-12-10 01:24:36 +0700

    initial commit2

--- src/main/java/org/example/Radio.java
+++ src/main/java/org/example/Radio.java
@@ -8,33 +8,6 @@ public class Radio {
         return currentStation;
     }
 
-    public class Conditioner {
-        private int currentTemperature;
-
-        public int getCurrentTemperature() {
-            return currentTemperature;
-        }
-
-        public void setCurrentTemperature(int newCurrentTemperature) {
-            if (newCurrentTemperature < 0) {
-                return;
-            }
-            if (newCurrentTemperature > 50) {
-                return;
-            }
-            currentTemperature = newCurrentTemperature;
-        }
-
-        public void increaseTemp50p() {
-            int target = currentTemperature * 3 / 2;
-            setCurrentTemperature(target);
-        }
-
-        public void setToMaxTemp() {
-            currentTemperature = 30;
-        }
-    }
-
     public void setCurrentStation(int stationNumber) {
         if (stationNumber >= 0 && stationNumber <= 9) {
             currentStation = stationNumber;
